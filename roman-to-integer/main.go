1eefbf84581ad43e984b0d04d4bf0d4410fe64ff
unknown <marchi775@gmail.com>
2025-07-09 21:33:55 -0300

    add roman to int

---
+++ roman-to-integer/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(romanToInt("MCMXCIV"))
+}
+
+func romanToInt(s string) int {
+	romanMap := make(map[rune]int)
+	romanMap['I'] = 1
+	romanMap['V'] = 5
+	romanMap['X'] = 10
+	romanMap['L'] = 50
+	romanMap['C'] = 100
+	romanMap['D'] = 500
+	romanMap['M'] = 1000
+	sum := 0
+	runes := []rune(s)
+
+	for i := 0; i < len(runes); i++ {
+		if i+1 < len(s) && romanMap[runes[i]] < romanMap[runes[i+1]] {
+			sum -= romanMap[runes[i]]
+		} else {
+			sum += romanMap[runes[i]]
+		}
+	}
+	return sum
+}
